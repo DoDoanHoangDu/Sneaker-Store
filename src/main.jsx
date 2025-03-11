77cd6d0e6ce1eb1f952d502f996f4541cf765c8f
TinTin2409 <163098502+TinTin2409@users.noreply.github.com>
2025-03-11 21:57:15 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import loginapp from './loginapp.jsx'
+import Loginapp from './loginapp.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Loginapp/>
   </StrictMode>,
 )
