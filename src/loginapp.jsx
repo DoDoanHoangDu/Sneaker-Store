6fcdf36cb2ad5c50d1043fab09c5c66304942313
TinTin2409 <163098502+TinTin2409@users.noreply.github.com>
2025-03-11 21:52:26 +0700

    Add files via upload

---
+++ src/loginapp.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import Login from './login.jsx';
+import Register from './register.jsx';
+
+function App() {
+    const [isRegister, setIsRegister] = useState(false);
+
+    const handleRegisterClick = () => {
+        setIsRegister(true);
+    };
+
+    const handleLoginClick = () => {
+        setIsRegister(false);
+    };
+
+    return (
+        <div>
+            {isRegister ? <Register onLoginClick={handleLoginClick} /> : <Login onRegisterClick={handleRegisterClick} />}
+        </div>
+    );
+}
+
+export default loginapp;
