4ad5a20bfd485584dce080e9913a11d0b95b9af2
HoangDu <dodoanhoangdu@gmail.com>
2025-04-01 09:55:03 +0700

    cart

--- src/components/ItemComponents/CartItem/CartItem.jsx
+++ src/components/ItemComponents/CartItem/CartItem.jsx
@@ -1,17 +1,62 @@
 import "./CartItem.css"
 import formatPrice from "../../../customHook/formatPrice"
+import { useState } from "react";
 function CartItem({item}) {
+    const [quantity, setQuantity] = useState(1);
+    const [tempQuantity, setTempQuantity] = useState("1");
+
+    const increaseQuantity = () => {
+        applyQuantityChange();
+        setQuantity(prevQuantity => prevQuantity + 1);
+        setTempQuantity((quantity + 1).toString());
+    };
+
+    const decreaseQuantity = () => {
+        applyQuantityChange();
+        setQuantity(prevQuantity => (prevQuantity > 1 ? prevQuantity - 1 : 1));
+        setTempQuantity((quantity > 1 ? quantity - 1 : 1).toString());
+    };
+
+    const handleTempQuantityChange = (e) => {
+        setTempQuantity(e.target.value);
+    };
+
+    const applyQuantityChange = () => {
+        const parsedValue = parseInt(tempQuantity, 10);
+        if (!isNaN(parsedValue) && parsedValue > 0) {
+            setQuantity(parsedValue);
+        } else {
+            setTempQuantity(quantity.toString());
+        }
+    };
+
+    const handleKeyDown = (e) => {
+        if (e.key === "Enter") {
+            applyQuantityChange();
+            e.target.blur();
+        }
+    };
     return(
         <div className="cart-item-container">
             <div className="cart-item">
                 <a href="/#" className="cart-item-left">
                     <img className="cart-item-image" src={item.image}></img>
                 </a>
+                <a className="cart-item-name">{item.name}</a> 
                 <div className="cart-item-right">
-                    <a className="cart-item-name">{item.name}</a> 
-                    <span className="cart-item-original-price">{formatPrice(item.originalPrice)}₫</span>
-                    <span className="cart-item-discount">-{Math.round(100*(1 - item.discountedPrice/item.originalPrice))}%</span>
                     <span className="cart-item-discounted-price">{formatPrice(item.discountedPrice)}₫</span>
+                    <span className="cart-item-original-price">{formatPrice(item.originalPrice)}₫</span>
+                    <br/>
+                    <div className="item-quantity-container">
+                        <button className="decrease-btn" onClick={decreaseQuantity}> - </button>
+                        <input 
+                            className="quantity-input" 
+                            value={tempQuantity} 
+                            onChange={handleTempQuantityChange} 
+                            onBlur={applyQuantityChange} 
+                            onKeyDown={handleKeyDown} />
+                        <button className="increase-btn" onClick={increaseQuantity}> + </button>
+                    </div>
                 </div>
             </div>
         </div>
